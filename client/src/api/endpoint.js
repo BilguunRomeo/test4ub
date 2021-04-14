e5931fc27563060c3149225dd0847512bb44624d
Munhgerel Bilguun <xxclairoxx@gmail.com>
2021-04-15 00:44:44 +0800

    sda

--- client/src/api/endpoint.js
+++ client/src/api/endpoint.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:4000/app/sendSurvey'
+  baseURL: 'http://localhost:5000/app/sendSurvey'
 })
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const routerUrls = require('./routes/routes')
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 dotenv.config()
 

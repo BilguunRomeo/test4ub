af8b8d161a536b58a673537565703c24d4a09149
Munhgerel Bilguun <xxclairoxx@gmail.com>
2021-04-15 01:39:46 +0800

    havarjiy

--- server.js
+++ server.js
@@ -27,7 +27,9 @@ const connectDB = async () => {
 };
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/client/dist')));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
 }
 
 connectDB()
